7e96eae3df03dcd5c7a83406d49d0d0c5cbcaecd
anthony <anthonyl26@nycstudents.net>
2022-10-12 00:55:43 -0400

    1

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = scan.nextLine();
+
+        System.out.println("String Length: " + str1.length());
+        System.out.println("First half: " + str1.substring(0,str1.length()/2));
+        System.out.println("Second half: " + str1.substring(str1.length()/2));
+
+        System.out.print("Enter second string: ");
+        String str2 = scan.nextLine();
+
+        if(str2.length()>str1.length()) {
+            System.out.println(str2 + " is larger than " + str1);
+        }
+        else if(str2.length()<str1.length()) {
+            System.out.println(str1 + " is larger than " + str2);
+        }
+
+        if(str1.compareTo(str2) > 0){
+            System.out.println(str2 + " is first alphabetically");
+        }
+        else if(str2.compareTo(str1) > 0){
+            System.out.println(str1 + " is first alphabetically");
+        }
+
+        if (str1.indexOf(str2) >= 0) {
+            System.out.println(str2 + " is found in " + str1 + " at index " + str1.indexOf(str2));
+        }
+        else if (str2.indexOf(str1) >= 0) {
+            System.out.println(str1 + " is found in " + str2+ " at index " + str2.indexOf(str1));
+        }
+        else{
+            System.out.println(str2 + " is NOT found in " + str1);
+        }
+
+    }
+}
